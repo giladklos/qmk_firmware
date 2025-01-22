629bce048827c876602eb6c54d237dbf392d3341
GiladWDS <gilad@wds-tech.com>
2025-01-22 14:18:19 +0200

    achordion updates

--- keyboards/idank/aztec42/keymaps/giladklos/keymap.c
+++ keyboards/idank/aztec42/keymaps/giladklos/keymap.c
@@ -76,6 +76,19 @@ bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
+bool achordion_eager_mod(uint8_t mod) {
+  switch (mod) {
+    case MOD_LSFT:
+    case MOD_RSFT:
+    case MOD_LCTL:
+    case MOD_RCTL:
+      return true;  // Eagerly apply Shift and Ctrl mods.
+
+    default:
+      return false;
+  }
+}
+
 // void keyboard_post_init_user(void) {
 //     // Set RGB Underglow to Swirl Effect
 //     rgblight_mode(RGBLIGHT_MODE_TWINKLE);
@@ -123,168 +136,110 @@ enum keymap_layers {
 #define PASTE LCTL(KC_V)
 #define CUT LCTL(KC_X)
 
-// uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
-//   return 200;
-// }
-
 // to build: qmk compile -kb crkbd/rev1 -km dvorak_42_key
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [LAYER_BASE] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P,
-        KC_DELETE,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_LSFT, HOME_A, HOME_S, HOME_D, HOME_F, KC_G, KC_H, HOME_J, HOME_K,
-        HOME_L, HOME_SCLN, KC_QUOT,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT,
-        KC_SLSH, KC_LSFT,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        L_MEDIA, L_MOUSE, L_NAV, L_NUM, L_SYM, L_FUN
-        //`--------------------------'  `--------------------------'
+  [LAYER_BASE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+   KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    KC_DELETE,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+   KC_LSFT,  HOME_A,  HOME_S,  HOME_D,  HOME_F,   KC_G,                         KC_H,   HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,   KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                          KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_LSFT,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        L_MEDIA, L_MOUSE, L_NAV,      L_NUM,  L_SYM,   L_FUN
+                                      //`--------------------------'  `--------------------------'
 
-        ),
+  ),
 
-    [LAYER_NUM] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        KC_TRNS, KC_LEFT_BRACKET, KC_7, KC_8, KC_9, KC_RIGHT_BRACKET, EQEQ, NEQ,
-        LSEQ, GTEQ, KC_LT, KC_GT,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_SEMICOLON, KC_4, KC_5, KC_6, KC_EQUAL, KC_PPLS, KC_PMNS,
-        KC_PAST, KC_PSLS, KC_PEQL, KC_UNDS,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_GRAVE, KC_1, KC_2, KC_3, KC_BACKSLASH, KC_LCBR, KC_LBRC,
-        KC_LPRN, KC_RPRN, KC_RBRC, KC_RCBR,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        KC_DOT, KC_0, KC_MINUS, XXXXXXX, XXXXXXX, XXXXXXX
-        //`--------------------------'  `--------------------------'
-        ),
+  [LAYER_NUM] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+    KC_TRNS, KC_LEFT_BRACKET,  KC_7,    KC_8,    KC_9,    KC_RIGHT_BRACKET,    EQEQ,    NEQ,     LSEQ,    GTEQ,    KC_LT,   KC_GT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_SEMICOLON,     KC_4,    KC_5,    KC_6,    KC_EQUAL,            KC_PPLS, KC_PMNS, KC_PAST, KC_PSLS, KC_PEQL, KC_UNDS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_GRAVE,         KC_1,    KC_2,    KC_3,    KC_BACKSLASH,        KC_LCBR, KC_LBRC, KC_LPRN, KC_RPRN, KC_RBRC, KC_RCBR,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_DOT,  KC_0, KC_MINUS,      XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
+  ),
 
-    [LAYER_SYM] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        KC_TRNS, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_COLN, KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_PIPE, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-        //`--------------------------'  `--------------------------'
-        ),
 
-    [LAYER_FUN] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        KC_TRNS, KC_F12, KC_F7, KC_F8, KC_F9, KC_PSCR, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_F11, KC_F4, KC_F5, KC_F6, KC_SCRL, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_F10, KC_F1, KC_F2, KC_F3, KC_PAUS, XXXXXXX, XXXXXXX,
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        KC_TRNS, KC_TRNS, KC_TRNS, XXXXXXX, XXXXXXX, XXXXXXX
-        //`--------------------------'            `--------------------------'
-        ),
+  [LAYER_SYM] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+    KC_TRNS, KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
+  ),
 
-    [LAYER_MOUSE] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, REDO, PASTE,
-        KC_MS_UP, CUT, UNDO, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CUT, KC_MS_LEFT,
-        KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2
-        //`--------------------------' `--------------------------'
-        ),
 
-    [LAYER_NAV] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, REDO, PASTE,
-        KC_UP, CUT, UNDO, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, CUT, KC_LEFT,
-        KC_DOWN, KC_RIGHT, XXXXXXX, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_HOME, KC_PGUP,
-        KC_PGDN, KC_END, KC_INS, KC_TRNS,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, KC_ENT, KC_BSPC, KC_DELETE
-        //`--------------------------'            `--------------------------'
-        ),
+  [LAYER_FUN] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                              ,-----------------------------------------------------.
+    KC_TRNS, KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SCRL,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------|                           |--------+--------+--------+--------+--------+--------|
+    KC_TRNS, KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,                               XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|            |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_TRNS,  KC_TRNS, KC_TRNS,              XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'            `--------------------------'
+  ),
 
-    [LAYER_MEDIA] = LAYOUT_split_3x6_3(
-        //,-----------------------------------------------------.
-        //,-----------------------------------------------------.
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, UG_TOGG, UG_NEXT,
-        UG_HUEU, UG_HUED, UG_SATU, UG_SATD,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MNXT, KC_MPLY,
-        KC_MPRV, XXXXXXX, UG_VALU, UG_VALD,
-        //|--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-        XXXXXXX, ARROW, CPPSTD, QMKPSSWD,
-        //|--------+--------+--------+--------+--------+--------+--------|
-        //|--------+--------+--------+--------+--------+--------+--------|
-        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-        //`--------------------------'  `--------------------------'
-        ),
+  [LAYER_MOUSE] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                                 ,-----------------------------------------------------.
+    XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,                                     REDO,    PASTE,   KC_MS_UP,   CUT,     UNDO,    KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                                 |--------+--------+--------+--------+--------+--------|
+    XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                        CUT,     KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                                 |--------+--------+--------+--------+--------+--------|
+    XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                  XXXXXXX, KC_MS_WH_LEFT, KC_MS_WH_DOWN, KC_MS_WH_UP, KC_MS_WH_RIGHT, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|                |--------+--------+--------+--------+--------+--------+--------|
+                                        XXXXXXX,  XXXXXXX, XXXXXXX,                  KC_MS_BTN1, KC_MS_BTN3, KC_MS_BTN2
+                                      //`--------------------------'                `--------------------------'
+  ),
 
-    /*
-    // empty layer
-    [15] = LAYOUT_split_3x6_3(
-    //,-----------------------------------------------------.
-    ,-----------------------------------------------------. KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //|--------+--------+--------+--------+--------+--------|
-    |--------+--------+--------+--------+--------+--------| KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //|--------+--------+--------+--------+--------+--------|
-    |--------+--------+--------+--------+--------+--------| KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    //|--------+--------+--------+--------+--------+--------+--------|
-    |--------+--------+--------+--------+--------+--------+--------| KC_TRNS,
-    KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
-                                        //`--------------------------'
-    `--------------------------'
-    ),
-    */
+  [LAYER_NAV] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                             ,-----------------------------------------------------.
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   REDO,    PASTE,   KC_UP,   CUT,     UNDO,    KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   CUT,     KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   KC_HOME,    KC_PGUP, KC_PGDN, KC_END,  KC_INS,  KC_TRNS,
+//|--------+--------+--------+--------+--------+--------+--------|                |--------+--------+--------+--------+--------+--------+--------|
+                                        XXXXXXX,  XXXXXXX, XXXXXXX,               KC_ENT, KC_BSPC, KC_DELETE
+                                      //`--------------------------'            `--------------------------'
+  ),
+  
+  [LAYER_MEDIA] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+  XXXXXXX,   XXXXXXX, XXXXXXX,   XXXXXXX, XXXXXXX, XXXXXXX,                    UG_TOGG,   UG_NEXT,  UG_HUEU,   UG_HUED,  UG_SATU,  UG_SATD,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+ XXXXXXX,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        KC_MNXT, KC_MPLY,  KC_MPRV,   XXXXXXX,  UG_VALU,  UG_VALD,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+ XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,   XXXXXXX,     XXXXXXX,                  XXXXXXX,  XXXXXXX,  XXXXXXX,   ARROW,  CPPSTD,  QMKPSSWD,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  /*
+  // empty layer
+  [15] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+    KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                      KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                        KC_TRNS,  KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS
+                                      //`--------------------------'  `--------------------------'
+  ),
+  */
+
 
 };
