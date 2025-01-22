879e500b6e5d821042e3781f8baf0df0f709ec27
GiladWDS <gilad@wds-tech.com>
2025-01-22 14:50:06 +0200

    no home row mods

--- keyboards/idank/aztec42/keymaps/giladklos/keymap.c
+++ keyboards/idank/aztec42/keymaps/giladklos/keymap.c
@@ -76,19 +76,10 @@ bool achordion_chord(uint16_t tap_hold_keycode, keyrecord_t* tap_hold_record,
   return achordion_opposite_hands(tap_hold_record, other_record);
 }
 
-bool achordion_eager_mod(uint8_t mod) {
-  switch (mod) {
-    case MOD_LSFT:
-    case MOD_RSFT:
-    case MOD_LCTL:
-    case MOD_RCTL:
-      return true;  // Eagerly apply Shift and Ctrl mods.
 
-    default:
-      return false;
-  }
+uint16_t achordion_timeout(uint16_t tap_hold_keycode) {
+  return 1000;
 }
-
 // void keyboard_post_init_user(void) {
 //     // Set RGB Underglow to Swirl Effect
 //     rgblight_mode(RGBLIGHT_MODE_TWINKLE);
@@ -141,9 +132,9 @@ enum keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    KC_DELETE,
+   KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    MOD_LGUI,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   KC_LSFT,  HOME_A,  HOME_S,  HOME_D,  HOME_F,   KC_G,                         KC_H,   HOME_J,  HOME_K,  HOME_L,  HOME_SCLN,   KC_QUOT,
+   KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,   KC_G,                         KC_H,   KC_J,  KC_K,  KC_L,  KC_SCLN,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
    KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                          KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_LSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
