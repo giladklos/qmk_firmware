b593c7db3208976bfbb1ffc7f4f0d0100a9748f6
GiladWDS <gilad@wds-tech.com>
2025-01-23 11:03:18 +0200

    minor keymap changes

--- keyboards/idank/aztec42/keymaps/giladklos/keymap.c
+++ keyboards/idank/aztec42/keymaps/giladklos/keymap.c
@@ -131,11 +131,11 @@ enum keymap_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [LAYER_BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    MOD_LGUI,
+   KC_TAB,   KC_Q,    KC_W,     KC_E,    KC_R,    KC_T,                         KC_Y,   KC_U,   KC_I,     KC_O,    KC_P,    KC_LGUI,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
    KC_LSFT,  KC_A,  KC_S,  KC_D,  KC_F,   KC_G,                         KC_H,   KC_J,  KC_K,  KC_L,  KC_SCLN,   KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                          KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_LSFT,
+   KC_LCTL,  KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                          KC_N,   KC_M,   KC_COMM,  KC_DOT,  KC_SLSH,   KC_LALT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                         L_MEDIA, L_MOUSE, L_NAV,      L_NUM,  L_SYM,   L_FUN
                                       //`--------------------------'  `--------------------------'
@@ -182,7 +182,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_MOUSE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                                 ,-----------------------------------------------------.
-    XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,                                     REDO,    PASTE,   KC_MS_UP,   CUT,     UNDO,    KC_TRNS,
+    XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,                                     REDO,    PASTE,   KC_MS_UP,   COPY,     UNDO,    KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                                 |--------+--------+--------+--------+--------+--------|
     XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                                        CUT,     KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT, XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                                 |--------+--------+--------+--------+--------+--------|
@@ -194,7 +194,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [LAYER_NAV] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                             ,-----------------------------------------------------.
-XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   REDO,    PASTE,   KC_UP,   CUT,     UNDO,    KC_TRNS,
+XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   REDO,    PASTE,   KC_UP,   COPY,     UNDO,    KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                                   CUT,     KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, KC_TRNS,
   //|--------+--------+--------+--------+--------+--------|                             |--------+--------+--------+--------+--------+--------|
